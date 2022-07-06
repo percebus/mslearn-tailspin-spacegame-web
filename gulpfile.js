81b0ad4ee75915442ccbc4fac2852c3d436296b7
JC Guerrero <JCystems@gMail.com>
2022-07-06 09:29:18 -0500

    feat(standard)

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,42 @@
-﻿/// <binding Clean='clean' />
-"use strict";
+/// <binding Clean='clean' />
+'use strict'
 
-const gulp = require("gulp"),
-      rimraf = require("rimraf"),
-      concat = require("gulp-concat"),
-      cleanCSS = require("gulp-clean-css"),
-      uglify = require("gulp-uglify");
+const gulp = require('gulp')
+const rimraf = require('rimraf')
+const concat = require('gulp-concat')
+const cleanCSS = require('gulp-clean-css')
+const uglify = require('gulp-uglify')
 
 const paths = {
-  webroot: "./Tailspin.SpaceGame.Web/wwwroot/"
-};
+  webroot: './Tailspin.SpaceGame.Web/wwwroot/'
+}
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.js = paths.webroot + 'js/**/*.js'
+paths.minJs = paths.webroot + 'js/**/*.min.js'
+paths.css = paths.webroot + 'css/**/*.css'
+paths.minCss = paths.webroot + 'css/**/*.min.css'
+paths.concatJsDest = paths.webroot + 'js/site.min.js'
+paths.concatCssDest = paths.webroot + 'css/site.min.css'
 
-gulp.task("clean:js", done => rimraf(paths.concatJsDest, done));
-gulp.task("clean:css", done => rimraf(paths.concatCssDest, done));
-gulp.task("clean", gulp.series(["clean:js", "clean:css"]));
+gulp.task('clean:js', done => rimraf(paths.concatJsDest, done))
+gulp.task('clean:css', done => rimraf(paths.concatCssDest, done))
+gulp.task('clean', gulp.series(['clean:js', 'clean:css']))
 
-gulp.task("min:js", () => {
-  return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+gulp.task('min:js', () => {
+  return gulp.src([paths.js, '!' + paths.minJs], { base: '.' })
     .pipe(concat(paths.concatJsDest))
     .pipe(uglify())
-    .pipe(gulp.dest("."));
-});
+    .pipe(gulp.dest('.'))
+})
 
-gulp.task("min:css", () => {
-  return gulp.src([paths.css, "!" + paths.minCss])
+gulp.task('min:css', () => {
+  return gulp.src([paths.css, '!' + paths.minCss])
     .pipe(concat(paths.concatCssDest))
     .pipe(cleanCSS())
-    .pipe(gulp.dest("."));
-});
+    .pipe(gulp.dest('.'))
+})
 
-gulp.task("min", gulp.series(["min:js", "min:css"]));
+gulp.task('min', gulp.series(['min:js', 'min:css']))
 
 // A 'default' task is required by Gulp v4
-gulp.task("default", gulp.series(["min"]));
+gulp.task('default', gulp.series(['min']))
